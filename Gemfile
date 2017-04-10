60f5796cb85866cff816fdcf3cec67fe483510f1
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-04-10 16:47:06 -0400

    add bootstrap, create home page

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
+
+gem 'bootstrap-sass', '3.3.6'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :meetings
   resources :books
   resources :users
-  root 'books#index'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
