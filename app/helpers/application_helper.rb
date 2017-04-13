386c8e8fb3af9f3d4c9451e92db503b3e69b2264
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-04-13 17:00:40 -0400

    format meeting time for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,18 @@ module ApplicationHelper
   end
 
   def format_time(time)
+    hour_int=time[0..1].to_i
+    am_or_pm=""
+    if hour_int>12
+      hour_int-=12
+      am_or_pm="PM"
+    elsif hour_int==0
+      hour_int=12
+      am_or_pm="AM"
+    else
+      am_or_pm="AM"
+    end
+
+    time_string= hour_int.to_s+time[2..4]+" "+am_or_pm      
   end
 end
